c996acf65b01843d2b6536c978a1b7f02db690c7
riteshbenjwal <riteshbenjwal7@gmail.com>
2023-02-12 22:08:42 +0530

    rabbitmq setup for subscribe message from booking service

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const { PORT } = require("./config/serverConfig");
+const { PORT, REMINDER_BINDING_KEY } = require("./config/serverConfig");
 const TicketController = require("./controllers/ticket-controller");
+const EmailService = require("./services/email-service");
 
 const jobs = require("./utils/job");
+const { subscribeMessage, createChannel } = require("./utils/messageQueue");
 
-const setupAndStartServer = () => {
+const setupAndStartServer = async () => {
   const app = express();
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-
   app.post("/api/v1/tickets", TicketController.create);
+
+  const channel = await createChannel();
+  subscribeMessage(channel, EmailService.subscribeEvents, REMINDER_BINDING_KEY);
+
   app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
     jobs();
